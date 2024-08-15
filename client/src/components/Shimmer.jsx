3baf878bcff1b11dba75b22e1c03c9dc6aa72824
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-15 19:18:58 +0530

    Added Shimmer

--- client/src/components/Shimmer.jsx
+++ client/src/components/Shimmer.jsx
@@ -2,9 +2,9 @@
 const Shimmer = () => {
   return (
     <div className="flex space-x-4">
-      <div className="w-24 h-24 bg-gray-200"></div>
-      <div className="w-24 h-24 bg-gray-200"></div>
-      <div className="w-24 h-24 bg-gray-200"></div>
+      <div className="w-48 h-48 bg-gray-200"></div>
+      <div className="w-48 h-48 bg-gray-200"></div>
+      <div className="w-48 h-48 bg-gray-200"></div>
     </div>
   );
 };
