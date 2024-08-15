ded5757f237a9a350dc0ccbc6a879134837cd0df
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-15 13:49:49 +0530

    Cors issue

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://book-management-assignment-frontend.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://book-management-assignment-frontend.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
