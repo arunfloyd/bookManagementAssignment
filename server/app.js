ce6ebd6cc3e4b1b05e30f115fbc4715ccce3054e
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-15 17:07:31 +0530

    OTP issue solving

--- server/app.js
+++ server/app.js
@@ -45,6 +45,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
 app.listen(process.env.PORT_DEV, () => {
   console.log(`Server is running on port ${process.env.PORT_DEV}`);
 });
