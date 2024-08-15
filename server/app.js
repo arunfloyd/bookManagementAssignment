31f94428e04bd6eb57073b05f13af3e7951aeec3
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-15 13:46:07 +0530

    Cors issue

--- server/app.js
+++ server/app.js
@@ -15,19 +15,25 @@ const app = express();
 connectDB();
 app.use(cookieParser());
 app.use(bodyParser.json());
-const allowedOrigins =
-  process.env.NODE_ENV === "production"
-    ? process.env.ALLOWED_ORIGINS_PRODUCTION.split(",")
-    : [...process.env.ALLOWED_ORIGINS_LOCAL.split(","), "http://localhost:3000"];
+const allowedOrigins = process.env.NODE_ENV === "production"
+  ? process.env.ALLOWED_ORIGINS_PRODUCTION.split(',')
+  : process.env.ALLOWED_ORIGINS_LOCAL.split(',');
 
 const corsOptions = {
-  origin: allowedOrigins, // This should be an array of allowed origins
+  origin: allowedOrigins,
   credentials: true,
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200
 };
-console.log('Allowed Origins:', allowedOrigins);
-
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://book-management-assignment-frontend.vercel.app/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(cors(corsOptions));
 app.use("/api/v1/language", languageRouter);
 app.use("/api/v1/author", authorRouter);
