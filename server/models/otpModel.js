e131422fb1757dae0fce3e699dc8a802b003c1a5
Arun Vinod K <arunvinod9497@gmail.com>
2024-08-15 17:12:33 +0530

    OTP issue getting ready

--- server/models/otpModel.js
+++ server/models/otpModel.js
@@ -1,15 +1,33 @@
 const mongoose = require("mongoose");
 
-const otpSchema = new mongoose.Schema({
-  otp: {
-    type: String,
-    required: true,
-    unique: true,
+const otpSchema = new mongoose.Schema(
+  {
+    otp: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+      index: { expires: '0' },
+    },
   },
-  expiresAt: {
-    type: Date,
-    required: true,
-  },
+  {
+    capped: {
+      size: 1024,
+      max: 1,
+      autoIndexId: true,
+    },
+  }
+);
+
+// Ensure only one document exists
+otpSchema.pre('save', async function(next) {
+  const OtpModel = this.constructor;
+  if (this.isNew) {
+    await OtpModel.deleteMany({});
+  }
+  next();
 });
 
 module.exports = mongoose.model("Otp", otpSchema);
